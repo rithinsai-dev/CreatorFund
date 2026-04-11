76510597f3781a2e6568ae65ebb127132b5b14a2
Pamerla Rithin <157945399+rithinsai-dev@users.noreply.github.com>
2026-04-11 14:35:43 +0530

    created the basic backend structure

---
+++ CreatorFund-backend/src/main/java/com/CreatorFund/CreatorFund/CreatorFundApplication.java
@@ -0,0 +1,13 @@
+package com.CreatorFund.CreatorFund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorFundApplication.class, args);
+	}
+
+}
---
+++ CreatorFund-backend/src/test/java/com/CreatorFund/CreatorFund/CreatorFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CreatorFund.CreatorFund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
